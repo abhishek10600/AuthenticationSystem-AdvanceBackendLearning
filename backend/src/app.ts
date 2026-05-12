f39beeddffac25e5d438c653a9f2e0878c38df41
abhishek10600 <abhi10sheksharma@gmail.com>
2026-05-13 00:34:38 +0530

    bolier plate code steup and logger setup.

---
+++ backend/src/app.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { env } from "./config/env.config.js";
+import { globalErrorHandler } from "./middlewares/error.middleware.js";
+import { AppError } from "./utils/common/errors/AppError.js";
+
+export const app = express();
+
+// app.set("trust proxy", 1);
+
+app.use(helmet());
+app.use(
+  cors({
+    origin: env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/health-check", (req: Request, res: Response) => {
+  return res.status(200).json({
+    success: true,
+    message: "Health is fine",
+  });
+});
+
+app.use((req, res, next) => {
+  next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
